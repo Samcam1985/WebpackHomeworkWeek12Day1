c8d397795eed034eab0a46595a7d26383262ad0c
Samantha Campbell <samantha.l.campbell1985@gmail.com>
2017-08-07 17:24:50 +0100

    Mvp met

---
+++ controllers/films.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var filmRouter = new express.Router();
+
+
+
+//since we don't have a database we'll use our front end models at the moment
+var films = require('../client/src/models/films')();
+var Film = require('../client/src/models/film');
+var Review = require('../client/src/models/review');
+
+filmRouter.post('/', function(req, res){
+  console.log(req.body)
+  films.push(req.body);
+  res.json(films);
+})
+
+filmRouter.put('/:id', function(req, res){
+  films[req.params.id] = req.body;
+  res.json(films);
+});
+
+filmRouter.delete('/:id', function(req, res){
+  films.splice(req.params.id, 1);
+  res.json(films);
+})
+
+filmRouter.get('/:id', function(req, res){
+  res.json(films[req.params.id]);
+})
+
+
+
+
+filmRouter.get('/', function(req, res){
+  res.json(films)
+})
+
+module.exports = filmRouter;
